f833c259376b2f8da90f3f876df42ad737347348
sgzed <wunaisong@163.com>
2020-06-16 09:11:27 +0800

    三角形 : e1

--- OpenGL_Test/graphics.cpp
+++ OpenGL_Test/graphics.cpp
@@ -74,18 +74,9 @@ int main()
 
 	glViewport(0, 0, SRC_WIDTH, SRC_HEIGHT);
 
-	//----------------------------------------------------
-	float vertices[] = {
-		-0.5f,-0.5f,0.0f,
-		 0.5f,-0.5f,0.0f,
-		 0.0f, 0.5f,0.0f,
-	};
 
-	unsigned int VBO;
-	glGenBuffers(1, &VBO);
-	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	//将用户数据绑定到缓冲
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	//----------------------------------------------------
+	// shader program
 
 	unsigned int vertexShader;
 	vertexShader = glCreateShader(GL_VERTEX_SHADER);
@@ -96,7 +87,6 @@ int main()
 	int  success;
 	char infoLog[512];
 	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
-
 	if (!success)
 	{
 		glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
@@ -108,17 +98,72 @@ int main()
 	fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
 	glShaderSource(fragmentShader, 1, &fshader, NULL);
 	glCompileShader(fragmentShader);
-
 	glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
-
 	if (!success)
 	{
 		glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
-		std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+		std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
 		return -1;
 	}
 
+	unsigned int shaderProgram = glCreateProgram();
 
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+	glLinkProgram(shaderProgram);
+	glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+	if (!success) {
+		glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+		std::cout << "ERROR::glLinkProgram\n" << infoLog << std::endl;
+		return -1;
+	}
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
+
+	//-------------------------------------------------------
+	// set up data 
+
+	float vertices[] = {
+		// first triangle
+		   -0.9f, -0.5f, 0.0f,  // left 
+		   -0.0f, -0.5f, 0.0f,  // right
+		   -0.45f, 0.5f, 0.0f,  // top 
+		   // second triangle
+			0.0f, -0.5f, 0.0f,  // left
+			0.9f, -0.5f, 0.0f,  // right
+			0.45f, 0.5f, 0.0f   // top 
+	};
+
+	unsigned int indices[] = {
+		0,1,3,		//第一个三角形
+		1,2,3		//2
+	};
+
+	unsigned int VAO, VBO,EBO;
+	glGenVertexArrays(1, &VAO);
+	glGenBuffers(1, &VBO);
+	glGenBuffers(1, &EBO);
+
+	//必须绑定顶点数组对象，否则不会绘制
+	glBindVertexArray(VAO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+
+	//将用户数据绑定到缓冲
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+	//链接顶点属性
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+
+	//这里是允许的，因为glVertexAttribPointer这里注册了VBO
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindVertexArray(0);
+
+	glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+
+	//---------------------------------------------
 
 	while (!glfwWindowShouldClose(window))
 	{
@@ -129,11 +174,21 @@ int main()
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 
+		glUseProgram(shaderProgram);
+		glBindVertexArray(VAO);
+		glDrawArrays(GL_TRIANGLES, 0, 6);
+		//glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
 		//------------------------------
 		glfwSwapBuffers(window);
 		//复习:windows消息机制
 		glfwPollEvents();
 	}
+	//释放资源
+	glDeleteVertexArrays(1, &VAO);
+	glDeleteBuffers(1, &VBO);
+	glDeleteBuffers(1, &EBO);
+	glDeleteProgram(shaderProgram);
 
 	glfwTerminate();
 	return 0;
