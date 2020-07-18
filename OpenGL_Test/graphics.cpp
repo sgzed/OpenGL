8fd8432b1d2c82cd141c586e7889a0a2d0833141
sgzed <wunaisong@163.com>
2020-07-18 15:08:41 +0800

    着色器类

--- OpenGL_Test/graphics.cpp
+++ OpenGL_Test/graphics.cpp
@@ -1,7 +1,9 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
+#include "shaderClass.h"
 #include <iostream>
+
 using std::endl;
 using std::cout;
 
@@ -15,19 +17,25 @@ const unsigned int SRC_HEIGHT = 600;
 
 const char* vshader = GET_STR(
 	#version 330 core \n
-	layout(location = 0) in vec3 aPos;;
+	layout(location = 0) in vec3 aPos;
+	layout(location = 1) in vec3 aColor;
+	out vec3 ourColor;
+
 	void main()
 	{
-		gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);
+		gl_Position = vec4(aPos, 1.0);
+		ourColor = aColor;
 	}
 );
 
 const char* fshader = GET_STR(
 	#version 330 core \n
 	out vec4 FragColor;
+	in vec3 ourColor;
+
 	void main()
 	{
-		FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);
+		FragColor = vec4(ourColor,1);
 	}
 );
 
@@ -77,66 +85,15 @@ int main()
 
 	//----------------------------------------------------
 	// shader program
-
-	unsigned int vertexShader;
-	vertexShader = glCreateShader(GL_VERTEX_SHADER);
-
-	glShaderSource(vertexShader, 1, &vshader, NULL);
-	glCompileShader(vertexShader);
-
-	int  success;
-	char infoLog[512];
-	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
-	if (!success)
-	{
-		glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
-		std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
-		return -1;
-	}
-
-	unsigned int fragmentShader;
-	fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-	glShaderSource(fragmentShader, 1, &fshader, NULL);
-	glCompileShader(fragmentShader);
-	glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
-	if (!success)
-	{
-		glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
-		std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
-		return -1;
-	}
-
-	unsigned int shaderProgram = glCreateProgram();
-
-	glAttachShader(shaderProgram, vertexShader);
-	glAttachShader(shaderProgram, fragmentShader);
-	glLinkProgram(shaderProgram);
-	glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
-	if (!success) {
-		glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
-		std::cout << "ERROR::glLinkProgram\n" << infoLog << std::endl;
-		return -1;
-	}
-	glDeleteShader(vertexShader);
-	glDeleteShader(fragmentShader);
-
+	Shader ourShader("3.3.shader.vs", "3.3.shader.fs");
+	
 	//-------------------------------------------------------
-	// set up data 
-
+	// set up vertex data (and buffer(s)) and configure vertex attributes
 	float vertices[] = {
-		// first triangle
-		   -0.9f, -0.5f, 0.0f,  // left 
-		   -0.0f, -0.5f, 0.0f,  // right
-		   -0.45f, 0.5f, 0.0f,  // top 
-		   // second triangle
-			0.0f, -0.5f, 0.0f,  // left
-			0.9f, -0.5f, 0.0f,  // right
-			0.45f, 0.5f, 0.0f   // top 
-	};
-
-	unsigned int indices[] = {
-		0,1,3,		//第一个三角形
-		1,2,3		//2
+		// positions         // colors
+		 0.5f, -0.5f, 0.0f,  1.0f, 0.0f, 0.0f,  // bottom right
+		-0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,  // bottom left
+		 0.0f,  0.5f, 0.0f,  0.0f, 0.0f, 1.0f   // top 
 	};
 
 	unsigned int VAO, VBO,EBO;
@@ -151,17 +108,21 @@ int main()
 
 	//将用户数据绑定到缓冲
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
-	//链接顶点属性
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	//链接顶点属性 
+	//position attribute
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
+
+	//color attribute
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+	glEnableVertexAttribArray(1);
 
 	//这里是允许的，因为glVertexAttribPointer这里注册了VBO
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
 	glBindVertexArray(0);
 
-	glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+	glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
 
 	//---------------------------------------------
 
@@ -174,9 +135,11 @@ int main()
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 
-		glUseProgram(shaderProgram);
+		ourShader.use();
+
+		//绘制三角形
 		glBindVertexArray(VAO);
-		glDrawArrays(GL_TRIANGLES, 0, 6);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
 		//glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
 		//------------------------------
@@ -188,7 +151,6 @@ int main()
 	glDeleteVertexArrays(1, &VAO);
 	glDeleteBuffers(1, &VBO);
 	glDeleteBuffers(1, &EBO);
-	glDeleteProgram(shaderProgram);
 
 	glfwTerminate();
 	return 0;
